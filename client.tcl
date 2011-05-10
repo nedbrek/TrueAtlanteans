45a537dae039df98d9b9a14a2b17b726ef0bd781
Edward Brekelbaum <nedbrek@yahoo.com>
2011-05-09 20:31:27 -0400

    Added db support    Changed draw to use db
    Created left and right frames
       Added text overview in left frame

--- client.tcl
+++ client.tcl
@@ -1,6 +1,8 @@
+package require sqlite3
+
 wm withdraw .
 
-set zoomLevels {
+set ::zoomLevels {
 	 3
 	 6
 	13
@@ -9,9 +11,38 @@ set zoomLevels {
 	45
 }
 
-set terrainColors {
+set ::monthNames {
+	January
+	February
+	March
+	April
+	May
+	June
+	July
+	August
+	September
+	October
+	November
+	December
+}
+
+set ::terrainColors {
+	jungle   #205020
 	mountain #704018
 	ocean    #000090
+	plain    #ffffc0
+	swamp    #a0a040
+}
+
+##############################################################################
+proc dGet {d k} {
+	return [string trim [dict get $d $k]]
+}
+
+proc calcTurnNo {m y} {
+	set mN [lsearch $::monthNames $m]
+
+	return [expr ($y-1)*12 + $mN + 1]
 }
 
 # use 1,2,rad3 triangles
@@ -62,25 +93,38 @@ proc plot_hex_num {obj x y} {
 	return $hexId
 }
 
-# draw the hexes described in region data
-proc drawRegion {w rdata} {
-	set hexId [plot_hex_num $w {*}[dict get $rdata Location]]
-	
-	set ttype  [dict get $rdata Terrain]
-	set tcolor [dict get $::terrainColors $ttype]
-
-	$w itemconfigure $hexId -fill $tcolor
+if {0} {
+	db eval {SELECT x, y, type FROM terrain} res {
+		puts -nonewline "x = $res(x) y = $res(y) "
+		puts "type $res(type)"
+	}
 }
 
-# draw all the regions in the turn data
-proc drawMap {w tdata} {
+# draw all the regions in the db data
+proc drawDB {w db} {
+	db eval {SELECT x, y, type FROM terrain} res {
+		set hexId [plot_hex_num $w $res(x) $res(y)]
+
+		$w itemconfigure $hexId -fill [dict get $::terrainColors $res(type)]
+	}
+}
+
+proc updateTerrain {db tdata} {
 	set regions [dict get $tdata Regions]
 	foreach r $regions {
-		drawRegion $w $r
+		set loc [dict get $r Location]
+		set x [lindex $loc 0]
+		set y [lindex $loc 1]
+		set ttype [dict get $r Terrain]
+		$db eval {INSERT OR REPLACE INTO terrain VALUES ($x, $y, $ttype);}
 
 		set exits [dict get $r Exits]
 		foreach {d e} $exits {
-			drawRegion $w $e
+			set loc [dict get $e Location]
+			set x [lindex $loc 0]
+			set y [lindex $loc 1]
+			set ttype [dict get $e Terrain]
+			$db eval {INSERT OR REPLACE INTO terrain VALUES ($x, $y, $ttype);}
 		}
 	}
 }
@@ -88,6 +132,30 @@ proc drawMap {w tdata} {
 #	set cx [$w canvasx $x]
 #	set cy [$w canvasy $y]
 #	set hexId [$w find closest $cx $cy]
+
+proc displayRegion {x y} {
+	set d $::region
+	set t .t.fL.tDesc
+	$t delete 1.0 end
+
+	set terrain [db eval {SELECT type FROM terrain WHERE x=$x AND y=$y;}]
+	$t insert end $terrain
+
+	$t insert end " ($x,$y) in [dGet $d Region]\n"
+
+	set city [dict get $d Town]
+	if {[llength $city]} {
+		$t insert end "contains [string trim [lindex $city 0]]"
+		$t insert end " \[[string trim [lindex $city 1]]\]\n"
+	}
+
+	$t insert end "[dGet $d Population] peasants "
+	$t insert end "([dGet $d Race]), \$[dGet $d MaxTax].\n"
+	$t insert end "------------------------------------\n"
+	$t insert end "The weather was [dGet $d WeatherOld] last month;\n"
+	$t insert end "it will be [dGet $d WeatherNew] next month.\n"
+	$t insert end "Wages: \$[dGet $d Wage] (Max: \$[dGet $d MaxWage]).\n"
+}
 
 # process user click on hex
 proc hexClick {w x y} {
@@ -121,7 +189,17 @@ proc hexClick {w x y} {
 
 	set hexTag [lindex $tags $i]
 	regexp {hex_([[:digit:]]+)_([[:digit:]]+)} $hexTag -> hx hy
-	puts "Hex $hx $hy"
+
+	displayRegion $hx $hy
+}
+
+proc loadData {filename} {
+	set tfile [open $filename]
+	set tdata [read $tfile]
+	close $tfile
+
+	set turnNo [calcTurnNo [dict get $tdata Month] [dict get $tdata Year]]
+	updateTerrain db $tdata
 }
 
 ##############################################################################
@@ -129,28 +207,42 @@ toplevel .t
 #bind .t <Destroy> {exit}
 wm title .t "True Atlantians"
 
-# need scrollbars to navigate
-scrollbar .t.canvasX -command ".t.screen xview" -orient horizontal
-scrollbar .t.canvasY -command ".t.screen yview" -orient vertical
+pack [frame .t.fR] -side right -fill both -expand 1
 
-set w [canvas .t.screen -bg white -xscrollcommand ".t.canvasX set" \
--yscrollcommand ".t.canvasY set" \
+# need scrollbars to navigate
+scrollbar .t.fR.canvasX -command ".t.fR.screen xview" -orient horizontal
+scrollbar .t.fR.canvasY -command ".t.fR.screen yview" -orient vertical
+
+set w [canvas .t.fR.screen -bg white -xscrollcommand ".t.fR.canvasX set" \
+-yscrollcommand ".t.fR.canvasY set" \
 -scrollregion "0 0 4000 6000"]
 
-pack .t.canvasX -side bottom -fill x
-pack .t.canvasY -side right  -fill y
-pack .t.screen  -side right  -fill both -expand true
+pack .t.fR.canvasX -side bottom -fill x
+pack .t.fR.canvasY -side right  -fill y
+pack .t.fR.screen  -side right  -fill both -expand 1
 
+pack [frame .t.fL] -side left -anchor nw
+pack [text .t.fL.tDesc -width 40 -height 8] -side top
+
+### bindings
 # canvas normally doesn't want focus
 bind $w <Enter> {focus %W}
+bind .t.fL.tDesc <Enter> {focus %W}
+
 # bind mousewheel to vertical scrolling
 bind $w <MouseWheel> {%W yview scroll [expr %D < 0 ? 1 : -1] units}
 
 # bind click
 bind $w <1> {hexClick %W %x %y}
 
-set tfile [open creport.3]
-set tdata [read $tfile]
-close $tfile
+########
+sqlite3 db "test1.db"
+# terrain table: (x, y) -> terrain type
+db eval {CREATE TABLE terrain (x not null, y not null, type not null, unique(x,y));}
 
-drawMap $w $tdata
+loadData 1/creport.3
+loadData 2/creport.3
+loadData 3/creport.3
+
+drawDB $w db
+
