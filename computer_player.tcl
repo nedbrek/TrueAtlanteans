7a5ea59952ee3a3fab8dd0fc63721c650788986c
Edward Brekelbaum <nedbrek@yahoo.com>
2017-08-26 20:58:26 -0500

    computer_player.tcl: Started ramp in first hex
    Moved first turn code into its own function
    Added avoid and behind to leader

--- computer_player.tcl
+++ computer_player.tcl
@@ -43,6 +43,109 @@ proc evaluateSituation {} {
 	return $ret
 }
 
+proc rampFirstHex {units} {
+	# TODO check to make sure we got out of the starting city (exit wasn't blocked)
+	# TODO calculate a good budget to use
+	set budget 3000
+
+	foreach {x y z unit_id name uid il ol} $units {}
+
+	set rdata [db eval {
+		SELECT id, turn, sells
+		FROM detail
+		WHERE x=$x AND y=$y AND z=$z
+		ORDER BY turn DESC LIMIT 1
+	}]
+	foreach {regionId turn sells} $rdata {}
+	if {$turn != $gui::currentTurn} {
+		puts "Error - no data for region $x $y $z!"
+		exit -1
+	}
+
+	set maxTax [db onecolumn {
+		SELECT tax FROM detail WHERE id=$regionId ORDER BY turn DESC LIMIT 1
+	}]
+
+	set taxersNeeded [expr {$maxTax / 50}]
+	if {$taxersNeeded == 0} {
+		puts "No tax in region!"
+		exit -1
+	}
+
+	# limit by cash on hand
+	set item  [lindex $sells 0]
+	set price [lindex $sells 1]
+	# TODO configure maintenance cost
+	set maxBuy [expr {$budget / ($price + 20)}]
+	set numBuy [expr {min($taxersNeeded, $maxBuy)}]
+	set claimAmt [expr {$numBuy * ($price + 10)}]
+
+	lappend ol "form 1" "name unit Guard" "claim $claimAmt"
+	lappend ol "buy $numBuy [lindex $item 1]"
+	lappend ol "study COMB"
+	lappend ol "end"
+	lappend ol "claim 50"
+	lappend ol "study OBSE"
+
+	db eval {
+		UPDATE units SET orders=$ol
+		WHERE id=$unit_id
+	}
+}
+
+proc pickStartDirection {units} {
+	set exits [list]
+
+	set res [db eval {
+		SELECT dir, dest
+		FROM nexus_exits
+	}]
+	foreach {dir dest} $res {
+		set ex_n [dict create]
+		dict set ex_n Dir $dir
+		dict set ex_n Loc $dest
+
+		foreach {x y z} $dest {}
+		set terrain [db eval {
+			SELECT type FROM terrain
+			WHERE x=$x and y=$y and z=$z
+		}]
+		dict set ex_n Terrain $terrain
+		dict set ex_n Score [dict get $atl_cp::terrain_priority $terrain]
+
+		lappend exits $ex_n
+	}
+
+	# find the best
+	set sorted_exits [lsort -index 7 $exits]
+	set e [lindex $sorted_exits 0]
+	set best_score [dict get $e Score]
+	set best_dir [dict get $e Dir]
+	foreach e [lrange $sorted_exits 1 end] {
+		if {$best_score < [dict get $e Score]} {
+			break
+		}
+		# TODO selection criteria
+	}
+
+	set best_loc [dict get $res $best_dir]
+	foreach {x y z} $best_loc {}
+	if {$y > 15} {
+		set dir2 nw
+	} else {
+		set dir2 se
+	}
+
+	foreach {x y z unit_id name uid il ol} $units {
+		lappend ol "behind 1" "avoid 1"
+		lappend ol [format {move %s %s} $best_dir $dir2]
+		db eval {
+			UPDATE units SET orders=$ol
+			WHERE id=$unit_id
+		}
+	}
+}
+
 proc createOrders {sitRep} {
 	set units [dGet $sitRep Units]
 	if {[dict get $sitRep State] == "start"} {
@@ -51,58 +154,11 @@ proc createOrders {sitRep} {
 		if {$zlevel == 0} {
 			# we're in the nexus
 			# choose an exit
-			set exits [list]
-
-			set res [db eval {
-				SELECT dir, dest
-				FROM nexus_exits
-			}]
-			foreach {dir dest} $res {
-				set ex_n [dict create]
-				dict set ex_n Dir $dir
-				dict set ex_n Loc $dest
-
-				foreach {x y z} $dest {}
-				set terrain [db eval {
-					SELECT type FROM terrain
-					WHERE x=$x and y=$y and z=$z
-				}]
-				dict set ex_n Terrain $terrain
-				dict set ex_n Score [dict get $atl_cp::terrain_priority $terrain]
-
-				lappend exits $ex_n
-			}
-
-			# find the best
-			set sorted_exits [lsort -index 7 $exits]
-			set e [lindex $sorted_exits 0]
-			set best_score [dict get $e Score]
-			set best_dir [dict get $e Dir]
-			foreach e [lrange $sorted_exits 1 end] {
-				if {$best_score < [dict get $e Score]} {
-					break
-				}
-				# TODO selection criteria
-			}
-
-			set best_loc [dict get $res $best_dir]
-			foreach {x y z} $best_loc {}
-			if {$y > 15} {
-				set dir2 nw
-			} else {
-				set dir2 se
-			}
-
-			foreach {x y z unit_id name uid il ol} $units {
-				lappend ol [format {move %s %s} $best_dir $dir2]
-				db eval {
-					UPDATE units SET orders=$ol
-					WHERE id=$unit_id
-				}
-			}
+			pickStartDirection $units
 			return
 		}
 		#else, exited nexus
+		rampFirstHex $units
 		return
 	}
 	#else post-start
